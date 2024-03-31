360c6991c4834a98e75058781d4f08485ba50fac
Artem Savchenko <armisav@gmail.com>
2024-03-31 19:31:17 +0700

    P-27: Remove element from array

---
+++ src/main/java/org/example/RemoveArrayElement.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class RemoveArrayElement {
+    public int removeElement(int[] nums, int val) {
+        int currentIndex = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int currentValue = nums[i];
+            if (val == currentValue) {
+                count++;
+            } else {
+                nums[currentIndex] = currentValue;
+                currentIndex++;
+            }
+        }
+        for (int i = currentIndex; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+        return nums.length - count;
+    }
+}
---
+++ src/test/java/org/example/RemoveArrayElementTest.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveArrayElementTest {
+    private static Stream<Arguments> testArraysMerge() {
+        return Stream.of(
+                Arguments.of(
+                        List.of(),
+                        1,
+                        List.of()
+                ),
+                Arguments.of(
+                        List.of(-111111, 0, 5),
+                        0,
+                        List.of(-111111, 5, 0)
+                ),
+                Arguments.of(
+                        List.of(-101, -100, 0, 1, 200, 201),
+                        -101,
+                        List.of(-100, 0, 1, 200, 201, 0)
+                ),
+                Arguments.of(
+                        List.of(1, 2, 3, 2, 4, 2),
+                        2,
+                        List.of(1, 3, 4, 0, 0, 0)
+                ),
+                Arguments.of(
+                        List.of(0, 1, 2, 2, 3, 0, 4, 2),
+                        2,
+                        List.of(0, 1, 3, 0, 4, 0, 0, 0)
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testArraysMerge(List<Integer> list1, Integer toDelete, List<Integer> expected) {
+        int[] array1 = list1.stream().mapToInt(i -> i).toArray();
+        int[] expectedArray = expected.stream().mapToInt(i -> i).toArray();
+
+
+        RemoveArrayElement removeArrayElement = new RemoveArrayElement();
+        int existingCount = removeArrayElement.removeElement(array1, toDelete);
+        assertArrayEquals(array1, expectedArray);
+
+        int expectedCount = (int) list1.stream()
+                .filter(num -> !num.equals(toDelete))
+                .count();
+        assertEquals(expectedCount, existingCount);
+    }
+}
